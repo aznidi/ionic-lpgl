3ccd3ef5ebbbcf2fb42c611bb979b551b3f1ebed
AZNIDI <devwithsas@gmail.com>
2026-04-04 23:05:53 +0000

    refactor: design and graphical ui and ux

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
   IonContent,
 } from '@ionic/angular/standalone';
 import { ButtonComponent } from '../button/button.component';
+import { NavHeaderComponent } from '../nav-header/nav-header.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ButtonComponent],
+  imports: [IonContent, ButtonComponent, NavHeaderComponent],
 })
 export class HomePage {
   constructor(private router: Router) {}
